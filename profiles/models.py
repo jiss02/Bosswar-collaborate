4ff171e4c688851ba7a9a172b0956f4388016031
eneun <wtcat@naver.com>
2019-07-16 23:44:32 +0900

    phone number validation edit

--- profiles/models.py
+++ profiles/models.py
@@ -1,12 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
-from phonenumber_field.modelfields import PhoneNumberField
+from django.core.validators import RegexValidator
 
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE)
     email = models.EmailField(null=True) 
-    phonenumber = PhoneNumberField(unique=True)
+    
+    phonenumber = models.CharField(
+        max_length=13,
+        unique=True,
+        validators=[
+            RegexValidator(
+                regex=r"\d{3}-\d{3,4}-\d{4}",
+                message='010-1234-5678 형식으로 입력하세요.',
+            )
+        ]
+    )
     point = models.IntegerField(default=500)
 
     def __str__(self):
