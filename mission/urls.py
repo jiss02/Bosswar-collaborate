059fd9e6b9836f940497d501db8449b365491381
jeongyeonchoi <woodchuck98@naver.com>
2019-07-18 20:21:36 +0900

    mission

--- mission/urls.py
+++ mission/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from . import views
 
+from django.contrib import admin
+from django.urls import path
+import mission.views
 
 urlpatterns = [
-    
+    path('admin/', admin.site.urls),
+    path('', mission.views.index, name='index'),
+    path('mission/<int:post_id>', mission.views.show, name='show'),
+    path('mission/new', mission.views.new, name='new'),
+    path('mission/postcreate', mission.views.postcreate, name='postcreate'),
+    path('crud/postdelete/<int:post_id>', mission.views.postdelete, name='postdelete'),
 ]
--- mission/views.py
+++ mission/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render,get_object_or_404
 from post.models import Post
+from .forms import PostForm
 
 # Create your views here.
 
 def index(request):
     posts = Post.objects.all()
     return render(request, 'mission/index.html',{'posts':posts})
+
+def new(request):
+    return render(request, 'mission/new.html')
+
+def postcreate(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('index')
+    else:
+        form = PostForm()
+        return render(request, 'mission/new.html', {'form': form})
+
+def postdelete(request, post_id):
+    post = get_object_or_404(Post, pk = post_id)
+    post.delete()
+    return redirect('index')
+
 
